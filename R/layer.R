288738dd34cd9e58744aa4b3b87cd8e2d15010d8
JayT <johannes.titz@gmail.com>
2017-08-07 16:23:25 +0200

    improve naming
    acts_p_avg is now called avg_act_inert, inert for inertia; it is basically the avg_act in a layer, but is not updated immediately; so it is inertial
    
    fbi and ffi were renamend to g_fbi and g_ffi, because these are conductances
    
    gi is now called g_i_gain, because it is a gain factor for g_i, nothing more
    
    in the constructor avg_act_n is now called n_act_lrgr_forty, number of units that have an activation larger than .4, because this is what it actually is; comment was also added
    
    get_acts and get_scaled_acts is now called get_unit_acts and get_unit_scaled_acts, to be more precise and not think that this gets the overall activation of the layer or something else
    
    cycle function description was updated; the input is now described as a vector, not an Nx1 matrix, because R will handle matrix-vector mulitiplication correctly, also added that ext_input is actually a conductance value (g_e), changed g_es to g_e_per_unit, it is a vector, this should be more readable
    
    change gc_i to g_i, what is c supposed to mean anyway? it is the inhibitory conductance, so we call it like that
    
    add to comment to improve cycle function
    
    clamp-cycpe parameter also changed to activations (instead of input)
    
    get_act_avgs also called get_unit_act_avgs now
    
    avg_s_with_m instead of avg_s_eff and m_in_s m_avg_prc_in_s_avg
    
    get_rel_avg_l now get_unit_avg_l_prc
    
    return get_act_avgs value is now a data frame so that cxn can use it more easily, and unit_id is added as well
    
    get_rel_avg_l now get_unit_avg_l_prc
    
    updt_avg_l now updt_unit_avg_l
    
    update reset description (random = F is standard now)
    
    remove get_avg_act, this is an active binding called avg_act
    
    add separators for fields, private...

--- R/layer.R
+++ R/layer.R
@@ -7,7 +7,7 @@
 #' @field recip_avg_act_n scaling factor for the outputs coming out from
 #' this layer, notice this is different from C++ version, only updated with
 #' updt_pct_act
-#' @field acts_p_avg time-averaged version of acts_avg, it is
+#' @field avg_act_inert time-averaged version of avg_act, it is
 #' updated only at the end of plus phases, so it is not a proper time
 #' average, see updt_pct_act.
 #' @field g_e_avg average excitatory conductance (net input) for all
@@ -16,153 +16,156 @@
 #' for all inputs coming to the n-th unit
 #' @field ce_wt contrast-enhanced version of wt matrix, ce_wt = SIG(wt)
 #' @field n number of units
-#' @field fbi feedback inhibition
-#' @field gi overall gain on inhibition
+#' @field g_fbi feedback inhibition
+#' @field g_i_gain overall gain on inhibition
 layer <- R6::R6Class("layer",
+  #public ----------------------------------------------------------------------
   public = list(
     # constructor
-    initialize = function(dims, gi = 2){
+    initialize = function(dims, g_i_gain = 2){
       if (length(c(dims)) == 2){
         self$n <- prod(dims)
         unit1 <- unit$new()
         # use cloning
         self$units <- lapply(seq(self$n), function(x) unit1$clone(deep = T))
-        # notice how the unit array is 1-D. The 2-D structure of the
-        # layer doesn't have meaning in this part of the code
       } else{
         stop("dims argument should be of the type c(rows, columns)")
       }
-      # recip_avg_act_n is initialized as a reciprocal function of number of
-      # units that are > 0.4 (avg_act_n)
-      private$acts_p_avg <- self$get_acts()
-      avg_act_n <- max(c(sum(self$get_acts() > 0.4), 1))
-      private$recip_avg_act_n <- 1 / (avg_act_n + 2)
-      private$fbi <- private$fb * self$acts_avg
+      # recip_avg_act_n is initialized with number of units that are > 0.4
+      # (n_act_lrgr_forty)
+      private$avg_act_inert <- self$avg_act
+      n_act_lrgr_forty <- max(c(sum(self$get_unit_acts() > 0.4), 1))
+      private$recip_avg_act_n <- 1 / (n_act_lrgr_forty + 2)
+      private$g_fbi <- private$g_fbi_gain * self$avg_act
       invisible(self)
     },
-    #' get_acts returns a vector with the activities of all units of a layer
+    #' get_unit_acts returns a vector with the activities of all units of a
+    #' layer
     #'
     #' @rdname layer
-    get_acts = function(){
+    get_unit_acts = function(){
       sapply(self$units, function(x) x$act)
     },
-    #' get_scaled_act returns a vector with the scaled activities of all units
-    #' of a layer, scaling is done with recip_avg_act_n, it changes only
+    #' get_unit_scaled_acts returns a vector with the scaled activities of all
+    #' units of a layer, scaling is done with recip_avg_act_n, a reciprocal
+    #' function of the number of active units
     #'
     #' @rdname layer
-    get_scaled_acts = function(){
-      private$recip_avg_act_n * self$get_acts()
+    get_unit_scaled_acts = function(){
+      private$recip_avg_act_n * self$get_unit_acts()
     },
     #' cycle iterates one time step with layer object
     #'
-    #' @param intern_input An Ix1 matrix, where I is the total number of inputs from
-    #' all layers. Each input has already been scaled by the recip_avg_act_n of its
-    #' layer of origin and by the wt_scale_rel factor.
-    #' @param ext_input An Nx1 matrix denoting inputs that don't come from another
-    #' layer, where N is the number of units in this layer. An empty matrix
-    #' indicates that there are no external inputs.
+    #' @param intern_input single combined vector with inputs from all layers.
+    #'   Each input has already been scaled by the recip_avg_act_n of its layer
+    #'   of origin and by the wt_scale_rel factor. The weight matrix is
+    #'   multiplied with this vector
+    #' @param ext_input vector with inputs not coming from another layer, with
+    #'   length equalling the number of units in this layer. If empty, no
+    #'   external inputs are processed. Note that this is basically an
+    #'   excitatory conductance value (g_e)
     #' @rdname layer
-    cycle = function(intern_input, ext_input = 0){
-      ## obtaining the g_es
-      g_es <- self$ce_wt %*% intern_input  # you use contrast-enhanced weights
-      if (!isempty(ext_input)) g_es <- g_es + ext_input
+    cycle = function(intern_input, ext_input = NULL){
+      ## obtaining the g_e_per_unit
+      g_e_per_unit <- self$ce_wt %*% intern_input # contrast enhanced weights are used
+      if (!isempty(ext_input)) g_e_per_unit <- g_e_per_unit + ext_input
       ## obtaining inhibition
-      private$g_e_avg <- mean(g_es)
-      ffi <- private$ff * max(c(private$g_e_avg - private$ff0, 0))
-      private$fbi <- private$fbi + private$fb_dt * (private$fb * self$get_avg_act() - private$fbi)
-      gc_i <- private$gi * (ffi + private$fbi)
+      private$g_e_avg <- mean(g_e_per_unit)
+      g_ffi <- private$g_ffi_gain * max(c(private$g_e_avg - private$g_ffi_thres, 0))
+      private$g_fbi <- private$g_fbi + private$g_fbi_dt *
+        (private$g_fbi_gain * self$avg_act - private$g_fbi)
+      g_i <- private$g_i_gain * (g_ffi + private$g_fbi)
       ## calling the cycle method for all units
-
-      Map(function(x, y, z) x$cycle(y, z), self$units, g_es, gc_i)
+      # todo: split cycle function, so that nxx1 call can be done with a
+      # functional, much faster!
+      Map(function(x, y, z) x$cycle(y, z), self$units, g_e_per_unit, g_i)
       invisible(self)
     },
 
-    #' clamp_cycle iterates one time step with layer object with clamped input (i.e
-    #' activation is instantenously set to input)
+    #' clamp_cycle iterates one time step with layer object with clamped
+    #' activations (i.e activations are instantenously set without dt)
     #'
-    #' @param input vector of activations that you want to clamp
+    #' @param activations vector of activations that you want to clamp
     #' @rdname layer
-    clamp_cycle = function(input){
-      self$units <- Map(function(x, y) x$clamp_cycle(y), self$units, input)
+    clamp_cycle = function(activations){
+      self$units <- Map(function(x, y) x$clamp_cycle(y), self$units, activations)
       # updating inhibition for the next cycle
-      private$fbi <- private$fb * self$get_avg_act()
+      private$g_fbi <- private$g_fbi_gain * self$avg_act
       invisible(self)
     },
 
-    #' get_act_avgs returns a list with the s,m,l averages in the layer as vectors,
-    #' notice that the ss average is not returned, and avg_l is not updated before
-    #' being returned
+    #' get_unit_act_avgs returns a list with the s,m,l averages in the layer as
+    #' vectors, notice that the ss average is not returned, and avg_l is not
+    #' updated before being returned
     #'
     #' @rdname layer
-    get_act_avgs = function(m_in_s, avg_l_lrn_min, avg_l_lrn_max){
+    get_unit_act_avgs = function(m_avg_prc_in_s_avg, avg_l_lrn_min,
+                                 avg_l_lrn_max){
       avg_s <- sapply(self$units, function(x) x$avg_s)
       avg_m <- sapply(self$units, function(x) x$avg_m)
       avg_l <- sapply(self$units, function(x) x$avg_l)
 
-      # obatining avg_s_eff
-      avg_s_eff <- m_in_s * avg_m + (1 - m_in_s) * avg_s
+      # obatining avg_s_with_m
+      avg_s_with_m <- m_avg_prc_in_s_avg * avg_m +
+        (1 - m_avg_prc_in_s_avg) * avg_s
 
       # obtaining avg_l_lrn
-      avg_l_lrn <- avg_l_lrn_min + self$get_rel_avg_l() *
+      # avg_l_lrn will be a percentage value between max and min
+      avg_l_lrn <- avg_l_lrn_min + self$get_unit_avg_l_prc() *
         (avg_l_lrn_max - avg_l_lrn_min)
 
-      list("avg_s" =  avg_s, "avg_m" = avg_m, "avg_l" = avg_l,
-           "avg_s_eff" = avg_s_eff, "avg_l_lrn" = avg_l_lrn)
+      data.frame(unit_id = 1:length(avg_s), "avg_s" = avg_s, "avg_m" = avg_m,
+                 "avg_l" = avg_l, "avg_s_with_m" = avg_s_with_m,
+                 "avg_l_lrn" = avg_l_lrn)
     },
 
-    #' get_rel_avg_l returns the relative values of avg_l, these are the dependent
-    #' variables rel_avg_l in all units used in latest XCAL
+    #' get_unit_avg_l_prc returns the percentage value of avg_l in the
+    #' range of the minimum and maximum values for avg_l specified with the
+    #' parameters
     #'
     #' @rdname layer
-    get_rel_avg_l = function(){
-      l_avg_rel <- sapply(self$units, function(x) x$rel_avg_l)
+    #'
+    # maybe move to private? ---------------------------------------------------
+    get_unit_avg_l_prc = function(){
+      sapply(self$units, function(x) x$avg_l_prc)
     },
 
-    #' updt_avg_l updates the long-term average (avg_l) of all the units in the
-    #' layer, usually done after a plus phase
+    #' updt_unit_avg_l updates the long-term average (avg_l) of all the units in
+    #' the layer, usually done after a plus phase
     #'
     #' @rdname layer
-    updt_avg_l = function(){
-
+    updt_unit_avg_l = function(){
       self$units <- lapply(self$units, function(x) x$updt_avg_l())
       invisible(self)
     },
 
-    #' updt_pct_act updates the acts_p_avg and recip_avg_act_n variables, these
-    #' variables update at the end of plus phases instead of cycle by cycle. This
-    #' version assumes full connectivity when updating recip_avg_act_n. If partial
-    #' connectivity were to be used, this should have the calculation in
-    #' WtScaleSpec::SLayActScale, in LeabraConSpec.cpp
+    #' updt_pct_act updates the avg_act_inert and recip_avg_act_n variables,
+    #' these variables update at the end of plus phases instead of cycle by
+    #' cycle. This version assumes full connectivity when updating
+    #' recip_avg_act_n. If partial connectivity were to be used, this should
+    #' have the calculation in WtScaleSpec::SLayActScale, in LeabraConSpec.cpp
     #'
     #' @rdname layer
     updt_pct_act = function(){
-      private$acts_p_avg <- private$acts_p_avg + private$acts_p_avg_dt *
-        (self$get_avg_act() - private$acts_p_avg)
-      r_avg_act_n <- max(round(private$acts_p_avg * private$n), 1)
-      private$recip_avg_act_n <- 1 / (r_avg_act_n + 2)
+      private$avg_act_inert <- private$avg_act_inert + private$avg_act_inert_dt *
+        (self$avg_act - private$avg_act_inert)
+      n_units_avg_act <- max(round(private$avg_act_inert * private$n), 1)
+      private$recip_avg_act_n <- 1 / (n_units_avg_act + 2)
       invisible(self)
     },
 
-    #' reset sets the activation of all units to a random value, and sets all
-    #' activation time averages to that value, used to begin trials from a random
-    #' stationary point, the activity values may also be set to zero
+    #' reset sets the activation and activation averages of all units to 0, used
+    #' to begin trials from a random stationary point, the activity values may
+    #' also be set to random values between .05 and .95
     #'
-    #' @param random logical variable, if TRUE set activation randomly between .05
-    #' and .95, if FALSE set activation to 0
+    #' @param random logical variable, if TRUE set activation randomly between
+    #'   .05 and .95, if FALSE set activation to 0
     #' @rdname layer
     reset = function(random = F){
       self$units <- lapply(self$units, function(x) x$reset(random = random))
       invisible(self)
     },
 
-    #' get_avg_act gets the value of acts_avg, the mean of unit activities for
-    #' the layer
-    #'
-    #' @rdname layer
-    get_avg_act = function(){
-      # get the value of acts_avg, the mean of unit activities
-      avg_acts <- mean(self$get_acts())
-    },
     #' set_ce_weights sets contrast enhanced weight values
     #'
     #' @rdname layer
@@ -170,39 +173,42 @@ layer <- R6::R6Class("layer",
       self$ce_wt <- 1 / (1 + (off * (1 - self$wt) / self$wt) ^ gain)
       invisible(self)
     },
+    # fields ----------------------
     n = NULL,          # number of units
     wt = NULL,         # An NxI weights matrix, where the n-th row has the
     # current wt values for all inputs coming to the n-th unit
     ce_wt = NULL,      # contrast-enhanced version of wt. ce_wt = SIG(wt).
     units = NULL        # an array with all the unit objs of the layer
   ),
+  # private --------------------------------------------------------------------
   private = list(
     # dynamic
 
-    recip_avg_act_n = NULL,  # The scaling factor for the outputs coming out
-    # from THIS layer. Notice this is different from C++
-    # version. Only updated with updt_pct_act.
-    acts_p_avg = NULL,
+    # recip_avg_act_n is the scaling factor for the outputs coming out from THIS
+    # layer. Notice this is different from C++ version. Only updated with
+    # updt_pct_act.
+    recip_avg_act_n = NULL,
+    avg_act_inert = NULL,
 
-    g_e_avg = 0,       # average net input for all units during last cycle
-    fbi = NULL,        # feedback inhibition
+    g_e_avg = 0,       # average g_e for all units during last cycle
+    g_fbi = NULL,      # feedback inhibition
 
     # constant
-    ff = 1,            # gain for feedforward inhibition
-    ff0 = 0.1,         # threshold for feedforward inhibition
-    fb = 0.5,          # gain for feedback inhibition
-    fb_dt = 1/1.4,     # time step for fb inhibition (fb_tau = 1.4)
-    gi = 2,            # overall gain on inhibition
-    acts_p_avg_dt = 0.01  # time step constant for updating acts_p_avg
+    g_ffi_gain = 1,          # gain for feedforward inhibition
+    g_ffi_thres = 0.1,       # threshold for feedforward inhibition
+    g_fbi_gain = 0.5,        # gain for feedback inhibition
+    g_fbi_dt = 1 / 1.4,      # time step for fb inhibition (fb_tau = 1.4)
+    g_i_gain = 2,            # overall gain on inhibition
+    avg_act_inert_dt = 0.01  # time step constant for updating avg_act_inert
   ),
 
   active = list(
     # dependent
-    # average activity of all units
-    #' acts_avg returns a vector with the activities of all units of a layer
+    #
+    #' avg_act returns the average activity of all units
     #'
     #' @rdname layer
-    acts_avg = function(){
-      mean(self$get_acts())
+    avg_act = function(){
+      mean(self$get_unit_acts())
     })
 )
