cb7813f273b6c4ebd626d60bb233970b3a1deed2
JayT <johannes.titz@gmail.com>
2017-08-14 15:19:48 +0200

    update documentation

--- R/unit.R
+++ R/unit.R
@@ -3,58 +3,52 @@ NULL
 
 #' Class to simulate a biologically realistic neuron
 #'
+#' This class simulates a biologically realistic neuron in the lebra framework. A layer has the field "units", which is a list of unit objects.
+#'
 #' @docType class
 #' @importFrom R6 R6Class
 #' @export
 #' @keywords data
-#' @return Object of \code{\link{R6Class}} with methods for calculating neuron activity changes. A layer consists of multiple unit objects.
+#' @return Object of \code{\link{R6Class}} with methods for calculating neuron activity changes
 #' @format \code{\link{R6Class}} object.
 #'
 #' @examples
-#' Lightning$new("http://localhost:3000/")
-#' Lightning$new("http://your-lightning.herokuapp.com/")
+#' unit$new()
+#' unit$cycle()
 #'
 #' @field act percentage activation ("firing rate") of the unit, which is sent
 #'   to other units, think of it as a percentage of how many neurons are active
 #'   in a microcolumn of 100 neurons
-#' @field g_e excitatory conductance, asymptotically approaches g_e_raw (see cycle method), aka net input
-#' @field v membrane potential in the range between 0 and 2, 0 is -100mV, 2 is 100mV, thus 1 is 0mV (v should usually be between 0 and 1)
-#' @field v_eq equilibrium membrane potential, which is not reset by spikes, it just
-#' keeps integrating
+#' @field g_e excitatory conductance, asymptotically approaches g_e_raw (see
+#'   cycle method), aka net input
+#' @field v membrane potential in the range between 0 and 2, 0 is -100mV, 2 is
+#'   100mV, thus 1 is 0mV (v should usually be between 0 and 1)
+#' @field v_eq equilibrium membrane potential, which is not reset by spikes, it
+#'   just keeps integrating
 #' @field spike a flag that indicates spiking threshold was crossed
-#' @field i_adapt adaptation current
-#' @field avg_ss super short-term running average activation, integrates over act
+#' @field i_adapt adaptation current like in the AdEx model
+#' @field avg_ss super short-term running average activation, integrates over
+#'   act
 #' @field avg_s short-term running average activation, integrates over avg_ss,
-#' represents plus phase learning signal
+#'   represents plus phase learning signal
 #' @field avg_m medium-term running average activation, integrates over avg_s,
-#' represents minus phase learning signal
+#'   represents minus phase learning signal
 #' @field avg_l long-term running average activation, integrates over avg_m,
-#' drives long-term floating average for self-organized learning learning
+#'   drives long-term floating average for self-organized learning learning
+#'@field avg_l_prc this is avg_l in percentage of minimum (default is 0.1) and maximum (default is 1.5)
 #'
 #' @section Methods:
 #' \describe{
 #'   \item{Documentation}{For full documentation of each method go to https://github.com/lightning-viz/lightining-r/}
-#'   \item{\code{new(serveraddress)}}{This method is used to create object of this class with \code{serveraddress} as address of the server object is connecting to.}
+#'   \item{\code{new()}}{Creates an object of this class with default parameters.}
 #'
-#'   \item{\code{sethost(serveraddress)}}{This method changes server that you are contacting with to \code{serveraddress}.}
-#'   \item{\code{createsession(sessionname = "")}}{This method creates new session on the server with optionally given name in \code{sessionname}.}
-#'   \item{\code{usesession(sessionid)}}{This method changes currently used session on the server to the one with id given in \code{sessionid} parameter.}
-#'   \item{\code{openviz(vizid = NA)}}{This method by default opens most recently created by this object visualization. If \code{vizid} parameter is given, it opens a visualization with given id instead.}
-#'   \item{\code{enableautoopening()}}{This method enables auto opening of every visualisation that you create since that moment. Disabled by default.}
-#'   \item{\code{disableautoopening()}}{This method disables auto opening of every visualisation that you create since that moment. Disabled by default.}
-#'   \item{\code{line(series, index = NA, color = NA, label = NA, size = NA, xaxis = NA, yaxis = NA, logScaleX = "false", logScaleY = "false")}}{This method creates a line visualization for vector/matrix with each row representing a line, given in \code{series}.}
-#'   \item{\code{scatter(x, y, color = NA, label = NA, size = NA, alpha = NA, xaxis = NA, yaxis = NA)}}{This method creates a scatterplot for points with coordinates given in vectors \code{x, y}.}
-#'   \item{\code{linestacked(series, color = NA, label = NA, size = NA)}}{This method creates a plot of multiple lines given in matrix \code{series}, with an ability to hide and show every one of them.}
-#'   \item{\code{force(matrix, color = NA, label = NA, size = NA)}}{This method creates a force plot for matrix given in \code{matrix}.}
-#'   \item{\code{graph(x, y, matrix, color = NA, label = NA, size = NA)}}{This method creates a graph of points with coordinates given in \code{x, y} vectors, with connection given in \code{matrix} connectivity matrix.}
-#'   \item{\code{map(regions, weights, colormap)}}{This method creates a world (or USA) map, marking regions given as a vector of abbreviations (3-char for countries, 2-char for states) in \code{regions} with weights given in \code{weights} vector and with \code{colormap} color (string from colorbrewer).}
-#'   \item{\code{graphbundled(x, y, matrix, color = NA, label = NA, size = NA)}}{This method creates a bundled graph of points with coordinates given in \code{x, y} vectors, with connection given in \code{matrix} connectivity matrix. Lines on this graph are stacked a bit more than in the \code{graph} function.}
-#'   \item{\code{matrix(matrix, colormap)}}{This method creates a visualization of matrix given in \code{matrix} parameter, with its contents used as weights for the colormap given in \code{colormap} (string from colorbrewer).}
-#'   \item{\code{adjacency(matrix, label = NA)}}{This method creates a visualization for adjacency matrix given in \code{matrix} parameter.}
-#'   \item{\code{scatterline(x, y, t, color = NA, label = NA, size = NA)}}{This method creates a scatterplot for coordinates in vectors \code{x, y} and assignes a line plot to every point on that plot. Each line is given as a row in \code{t} matrix.}
-#'   \item{\code{scatter3(x, y, z, color = NA, label = NA, size = NA, alpha = NA)}}{This method creates a 3D scatterplot for coordinates given in vectors \code{x, y, z}.}
-#'   \item{\code{image(imgpath)}}{This method uploads image from file \code{imgpath} to the server and creates a visualisation of it.}
-#'   \item{\code{gallery(imgpathvector)}}{This method uploads images from vector of file paths \code{imgpathvector} to the server and creates a gallery of these images.}}
+#'   \item{\code{cycle(g_e_raw, g_i)}}{Cycles 1 ms with given excitatory conductance  \code{g_e_raw} and inhibitory conductance \code{g_i}. Excitatory conductance depends on the weights to other units and the activity of those other units. Inhibitory conductance depends on feedforward and feedback inhibition. See layer cycle method.}
+#'
+#'   \item{\code{clamp_cycle(activation)}}{Clamps the value of \code{activation} to the \code{act} variable of the unit without any time integration. Then updates averages. This is usually done when presenting external input.}
+#'
+#'   \item{\code{updt_avg_l()}}{This method updates the variable \code{avg_l}. This usually happens before the weights are changed in the network (after the plus phase), and not every cycle. It tends to move towards a constant minium avg_l (0.1) if avg_m is smaller than 0.2; if it is larger, than it will tend to go to avg_m}
+#'
+#'   \item{\code{nxx1(x)}}{Calculates the activation of a unit as a function of v or g_e and their thresholds}}
 
 unit <- R6::R6Class("unit",
   # public ---------------------------------------------------------------------
@@ -126,12 +120,6 @@ unit <- R6::R6Class("unit",
     },
 
     updt_avg_l = function(){
-        # This fuction updates the long-term average "avg_l"
-        # u = this unit
-        # Based on the description in:
-        # https://grey.colorado.edu/ccnlab/index.php/#Leabra_Hog_Prob_Fix#Adaptive_Contrast_Impl
-        # it tends to move towards the minium avg_l (0.1) if avg_m is smaller
-        # 0.2 if it is larger, than it will tend to got to avg_m
         if (self$avg_m > 0.2){
           self$avg_l <- self$avg_l + private$avg_l_dt *
             (private$avg_l_max - self$avg_m)
