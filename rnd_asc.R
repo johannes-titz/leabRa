62970f92e1bbaa7a6e17f3675b943f0724b2a593
JayT <johannes.titz@gmail.com>
2017-08-16 12:59:33 +0200

    change updt_pct_act to updt_recip_avg_act_n

--- rnd_asc.R
+++ rnd_asc.R
@@ -111,7 +111,7 @@ for (epoch in seq(n_epochs)) {
             net$cycle(inputs, clamp_inp = 1)
         }
 
-        net$updt_pct_act() # update averages used for net input scaling
+        net$updt_recip_avg_act_n() # update averages used for net input scaling
 
         #+++++++ LEARNING +++++++
         net <- net$chg_wt()  # updates the avg_l vars and applies XCAL learning
