641238858ec7eea1c5d78dff8ec9b9f1605b657b
JayT <johannes.titz@gmail.com>
2017-08-29 15:02:18 +0200

    test orig example

--- rnd_asc.R
+++ rnd_asc.R
@@ -13,7 +13,7 @@ cosine <- function(x, y){
 # point, layers are either fully connected or unconnected
 
 # 1.1) Set the dimensions of the layers specifying 3 layers and their dimensions
-dim_lays <- list(c(1, 5), c(5, 4), c(5, 1))
+dim_lays <- list(c(1, 25), c(1, 100), c(1, 25))
 
 # 1.2) Specify connectivity between layers
 cxn <- matrix(c(0, 0, 0,
@@ -26,21 +26,28 @@ cxn <- matrix(c(0, 0, 0,
 # entries of row i.  The network constructor will normalize this matrix so that
 # if there are non-zero entries in a row, they add to 1.
 
-net <- network$new(dim_lays, cxn)
+net <- network$new(dim_lays, cxn, g_i_gain = c(2, 2, 2))
 
 inputs_plus <- net$create_inputs(which_layers = c(1, 3),
-                              n_inputs = 5, prop_active = .3)
+                              n_inputs = 15, prop_active = .3)
 
 # remove the inputs for the output layer for minus phase
 inputs_minus <- lapply(inputs_plus, function(x) {x[3] <- list(NULL); return(x)})
 n_epochs <- 10
-outs <- lapply(seq(n_epochs), function(x) net$learn_error_driven(inputs_minus, inputs_plus, lrate = seq(0.8, 0.2, length.out = n_epochs)))
+
+outs <- lapply(seq(n_epochs), function(x) net$learn_error_driven(inputs_minus, inputs_plus, lrate = 0.8))
 
 # calc cosine
 
+extract_list_number = function(x, y){
+  lapply(x, function(x) x[[y]])
+}
+
+lapply(outs, extract_list_number, 3)
+
 overall_cosine <- function(outs, inputs_plus, layer){
-  outs_layer <- plyr::llply(outs[layer,], rbind)
-  inputs_plus_layer <- lapply(inputs_plus, function(x) x[[layer]])
+  outs_layer <- extract_list_number(outs, layer)
+  inputs_plus_layer <- extract_list_number(inputs_plus, layer)
   mean(unlist(Map(function(x, y) 1 - cosine(x, y), outs_layer, inputs_plus_layer)))
 }
 
