7625b000235bf1f68e7fcf050d272f7876491b68
Johannes Titz <johannes.titz@gmail.com>
2017-09-16 12:42:15 +0200

    change .95 and .05 to 1 / 0, formatting

--- R/data.R
+++ R/data.R
@@ -1,13 +1,12 @@
 #' Small example data set for self-organized artifical neural networks
 #'
 #' A small dataset describing 10 animals represented by 6 features that are
-#' either present (0.95) or absent (0.05) for demonstrating self-organized
-#' learning in artificial neural networks.
+#' either present (1) or absent (0) for demonstrating self-organized learning in
+#' artificial neural networks.
 #'
 #' @format A data frame with 10 rows and 6 variables describing 10 different
-#' animals with 6 feature vectors that are either present (0.95) or absent
-#' (0.05).
+#'   animals with 6 feature vectors that are either present (1) or absent (0).
 #'
 #' @source Knight, K. (1990). Connectionist ideas and algorithms.
-#' \emph{Communications of the ACM}, \emph{33}(11), 59–74.
+#'   \emph{Communications of the ACM}, \emph{33}(11), 59–74.
 "animals"
